3306480294c5784d1663ba90bc8e6322508c2722
notkatsa <pnkatsa@gmail.com>
2024-01-21 04:54:32 +0200

    rm some files

--- teacher_old/js/change_pages_teachers.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("button-grades1").onclick = function () {
-    location.href = "./teachergrades.html"
-}
-document.getElementById("button-info1").onclick = function () {
-    location.href = "./teacherinfo.html"
-}
-document.getElementById("button-courses1").onclick = function () {
-    location.href = "./teachercourses.html"
-}
